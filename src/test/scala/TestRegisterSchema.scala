43ede2f0187fb859a85f8352f0a4e9a2ae669a05
EcemPangal <48175699+ecemce@users.noreply.github.com>
2021-08-17 18:43:46 +0300

    Update TestRegisterSchema.scala

--- src/test/scala/TestRegisterSchema.scala
+++ src/test/scala/TestRegisterSchema.scala
@@ -4,7 +4,7 @@ import com.ttgint.apman.AnomalyDetector
 import org.scalatest.FunSuite
 
 class TestRegisterSchema extends FunSuite {
-  val subject = "APMANANOMALY_anomaly-value"
+  val subject = "*************"
   test("PrintSchema") {
     val schema = AvroSchema[anomaly]
     println(schema.toString(true))
